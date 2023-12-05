e80ea79317e1077e91b5007ccce083a1bd56dc03
ryan910707 <ryanke91@mail.com>
2023-12-05 23:51:39 +0800

    finish lab4

---
+++ lab4/test.sh
@@ -0,0 +1,6 @@
+# time ./lab4_cp /home/pp23/share/lab4/testcases/large-candy.png ./ans.png
+./lab4 /home/pp23/share/lab4/testcases/large-candy.png ./result.png
+png-diff result.png ans.png
+rm result.png
+
+
